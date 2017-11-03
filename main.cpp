04604ebc7217aca9ff75d3804492d5b2aa0975a4
czydbb <cai.zhenyu.2014@gmail.com>
2017-11-03 13:25:56 +0000

    Integrated getError into fix error. write the neighbour function for stabliser and use it to implement another stabiliserUpdate algorithm.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <cmath>
 #include <cassert>
 #include <array>
+#include <set>
+#include "PerfectMatching.h"
+//To make perfectmatching.h to work, we need to first delete example.cpp in blossom_dir, then we also cannot use the
+//triangle package as suggested due to lack of X11. We use import project in Clion to rewrite Cmake. Remember to exclude
+//the unwanted files.
 
 template <class InputType> // printMatrix can be printed without specifying InputType, this is done using implicit instantiation
 void printMatrix(InputType M){
@@ -43,8 +48,22 @@ public:
         return code[row][col];
     }
 
+    std::array<int, 2> idxTransform(int row, int col){
+        row = (row% n_row + n_row)%n_row;
+        col = (col% n_col + n_col)%n_col;
+        return {row, col};
+    }
+
     void printCode(){
         printMatrix(code);
+    }
+    void reset(){
+        for(int i = 0; i < n_row; i++){
+            code[i].resize(n_col);
+            for(int j = 0; j < n_col; j++){
+                code[i][j] = (ErrorType)0;
+            }
+        }
     }
 };
 
@@ -84,20 +103,20 @@ public:
 
 };
 
-//enum StabiliserType{
-//    X_MEASUREMENT = 1,
-//    Z_MEASUREMENT= -1
-//};
+enum StabiliserType{
+    X_STB = 1,
+    Z_STB= -1
+};
 
 //boolean 0 denote no error, 1 denotes error.
 class Stabiliser: public Code<int>{
 public:
+    StabiliserType stabiliser_type;
     std::vector<std::array<int,2>> error_locations;
-//    StabiliserType stabiliser_type;
     
 public:
-    Stabiliser(int n_row, int n_col): Code(n_row, n_col){}
-//    Stabiliser(int n_row, int n_col, StabiliserType stabiliser_type): Code(n_row, n_col), stabiliser_type(stabiliser_type){}
+//    Stabiliser(int n_row, int n_col): Code(n_row, n_col){}
+    Stabiliser(int n_row, int n_col, StabiliserType stabiliser_type): Code(n_row, n_col), stabiliser_type(stabiliser_type){}
     void induceError(double error_prob){
         assert(error_prob <= 1);
         std::random_device rd;
@@ -112,38 +131,58 @@ public:
             }
         }
     }
-    void getError(){
+
+    PerfectMatching* getErrorMatching(){
+
         for (int i = 0; i < n_row; i++) {
             for (int j = 0; j < n_col; j++) {
                 if (code[i][j] != 0) {
-                    std::array<int, 2> loc = {i,j};
-                    error_locations.push_back(loc);}
+                    error_locations.push_back({i,j});}
             }
         }
-    }
 
-    void printError(){
-        this->getError();
-        for (auto error: error_locations) printf("(%d, %d) ", error[0], error[1]);
-        printf("\n");
-    }
+        int n_error = error_locations.size();
+        std::vector <int> error_label;
+        for (int k = 0; k < n_error; ++k) error_label.push_back(k);
 
-    std::vector<int> getErrorDistance(){
-        this->getError();
-        int size = error_locations.size();
-        std::vector<std::vector<int>> error_distance;
-        error_distance.resize(size);
-        for (int i = 0; i < size; ++i) {
-            error_distance[i].resize(size);
+        int n_edges = n_error*(n_error-1)/2;
+        PerfectMatching *pm = new PerfectMatching(n_error, n_edges);
+
+        struct PerfectMatching::Options options;
+        options.verbose = false;
+        pm->options = options;
+
+        for (int i = 0; i < n_error; ++i) {
             for (int j = 0; j < i ; ++j) {
-                int d = abs(error_locations[i][0] - error_locations[j][0]) +
-                        abs(error_locations[i][1] - error_locations[j][1]);
-                error_distance[i][j] = d;
-                error_distance[j][i] = d;
+                int d;
+                d = std::min(abs(error_locations[i][0] - error_locations[j][0]),
+                             n_row - abs(error_locations[i][0] - error_locations[j][0])) +
+                    std::min(abs(error_locations[i][1] - error_locations[j][1]),
+                             n_col - abs(error_locations[i][1] - error_locations[j][1]));
+                pm->AddEdge(error_label[i], error_label[j], d);
             }
-            error_distance[i][i] = 0;
         }
+        pm->Solve();
+
+        return pm;
     }
+    std::array<int,2> neighbour(const char direction, int row, int col) {
+        std::array<int, 2> nb_pos;
+        if (stabiliser_type == X_STB) {
+            if (direction == 'N') nb_pos = {2 * row - 1, col};
+            else if (direction == 'S') nb_pos = {2 * row + 1, col};
+            else if (direction == 'W') nb_pos = {2 * row, col - 1};
+            else if (direction == 'E') nb_pos = {2 * row, col};
+        }
+        if (stabiliser_type == Z_STB) {
+            if (direction == 'N') nb_pos = {2 * row, col};
+            else if (direction == 'S') nb_pos = {2 * row + 2, col};
+            else if (direction == 'W') nb_pos = {2 * row + 1, col};
+            else if (direction == 'E') nb_pos = {2 * row + 1, col + 1};
+        }
+        return nb_pos;
+    }
+
 };
 
 class SurfaceCode{
@@ -156,8 +195,8 @@ public:
     Stabiliser stabiliserZ;
 
 public:
-    SurfaceCode(int n_row, int n_col): data(n_row, n_col), stabiliserX(n_row/2, n_col), stabiliserZ(n_row/2, n_col){}
-
+    SurfaceCode(int n_row, int n_col): data(n_row, n_col), stabiliserX(n_row/2, n_col, X_STB),
+                                                           stabiliserZ(n_row/2, n_col, Z_STB){}
 
     //here assume row 0 is X stabiliser, row 1 is Z stabliser, etc.
     void stabiliserUpdate(){
@@ -189,6 +228,49 @@ public:
         }
     }
 
+    void stabiliserUpdateSlow(){
+        std::array<char, 4> direction_array= {'N', 'S', 'E', 'W'};
+        int nX, nZ;
+        std::array<int, 2> pos;
+        for (int i = 0; i < stabiliserX.n_row; ++i) {
+            for (int j = 0; j < stabiliserX.n_col; ++j) {
+                nX = 0;
+                nZ = 0;
+                for (char direction: direction_array){
+                    pos = stabiliserX.neighbour(direction, i, j);
+                    if (data(pos[0],pos[1]) == Z_ERROR or data(pos[0],pos[1]) == Y_ERROR) nX++;
+                    pos = stabiliserZ.neighbour(direction, i, j);
+                    if (data(pos[0],pos[1]) == X_ERROR or data(pos[0],pos[1]) == Y_ERROR) nZ++;
+                }
+                if (nX%2 == 1) stabiliserX(i,j) = 1;
+                else stabiliserX(i,j) = 0;
+                if (nZ%2 == 1) stabiliserZ(i,j) = 1;
+                else stabiliserZ(i,j) = 0;
+            }
+        }
+    }
+
+
+    void fixError(){
+        int n_error = stabiliserX.error_locations.size();
+        PerfectMatching* pm = stabiliserX.getErrorMatching();
+        std::vector <int> error_label;
+        for (int k = 0; k < n_error; ++k) error_label.push_back(k);
+        std::set<int>error_corrected = {}; //std::array are implicitly copied.
+        for (int error: error_label){
+            if (error_corrected.find(error) == error_corrected.end()) { // equivalent to if error is not in error_corrected
+                int paired_error = pm->GetMatch(error);
+//                std::array<int,2>
+
+
+//                code[error_locations[error][0]][error_locations[error][1]] ^= 1;
+//                code[error_locations[paired_error][0]][error_locations[paired_error][1]] ^= 1;
+//                error_corrected.insert(error); //error is already iterated over, hence no need to check.
+                error_corrected.insert(paired_error);
+            }
+        }
+    }
+
 //red: 31, grn: 32, yel: 33, blu: 34, mag: 35, cyn: 36, wht: 37
     void printSurfaceCode(){
         for (int i = 0; i < data.n_row; i++) {
@@ -204,6 +286,7 @@ public:
             }
             printf("\n");
         }
+        printf("\n");
     }
 
 
@@ -220,10 +303,24 @@ int main() {
     c.data.induceError(0.2, Z_ERROR);
     c.data.printCode();
     c.stabiliserUpdate();
-    c.stabiliserX.printError();
-    c.stabiliserX.printCode();
-    c.stabiliserZ.printCode();
     c.printSurfaceCode();
+//    std::array<int,2> pos = c.stabiliserX.neighbour('N', 0, 0);
+//    printf("(%d, %d)", pos[0], pos[1]);
+    c.stabiliserX.reset();
+    c.stabiliserZ.reset();
+    c.stabiliserUpdateSlow();
+    c.printSurfaceCode();
+//    c.stabiliserX.printError();
+//    c.stabiliserX.printCode();
+//    c.stabiliserZ.printCode();
+//    c.printSurfaceCode();
+
+
+//    c.stabiliserX.fixError();
+//    c.stabiliserZ.fixError();
+//    c.stabiliserX.printCode();
+//    c.stabiliserZ.printCode();
+//    c.printSurfaceCode();
 }
 
 
